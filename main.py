986bbed1fe2afc0fc28b7ebba02048f221ddf36c
IsHYuhi <yuhiregend@icloud.com>
2020-01-14 15:23:00 +0900

    revert

--- main.py
+++ main.py
@@ -40,11 +40,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    string = 'ブランチテストです'
     line_bot_api.reply_message(
         event.reply_token,
-        #TextSendMessage(text=event.message.text)
-        TextSendMessage(text=string))
+        TextSendMessage(text=event.message.text)
+        )
 
 
 if __name__ == "__main__":
