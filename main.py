d8efddd4d1e76e6fd1bca371f88fe6a3f2ed4493
IsHYuhi <yuhiregend@icloud.com>
2020-01-14 21:12:20 +0900

    db operation test in heroku

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ from linebot.models import (
 )
 import os
 
+import sqlite3
+
+dbname = 'info.db'
 app = Flask(__name__)
+conn = sqlite3.connect(dbname)
+c = conn.cursor()
 
 #環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
@@ -40,9 +45,24 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if event.message.text == '登録':
+        sql = 'insert into userinfo (id, name, height, weight, dateofbirth, personality) values (?,?,?,?,?,?)'
+        info = (1, "michael", 164, 70, "1998年5月6日", "二重国籍")
+        conn.execute(sql, info)
+
+        conn.commit()
+
+        select_sql = 'select * from userinfo'
+
+        c.execute(select_sql)
+        result = c.fetchone()
+        conn.close()
+        message =''
+        for text in result:
+            message=message+str(text)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=message)
         )
 
 
