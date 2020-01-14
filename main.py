911a088062844f93cd5deb44857b217d6ae96e2d
IsHYuhi <yuhiregend@icloud.com>
2020-01-14 22:40:18 +0900

    add select option function

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import os
 
 import sqlite3
 app = Flask(__name__)
-
+STATUS = ''
 
 #環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
@@ -42,38 +42,78 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # if event.message.text == '登録':
-    #     sql = 'insert into userinfo (id, name, height, weight, dateofbirth, personality) values (?,?,?,?,?,?)'
-    #     info = (1, "michael", 164, 70, "1998年5月6日", "二重国籍")
-    #     conn.execute(sql, info)
+    global STATUS
+    if event.message.text == '登録':
+        STATUS = '登録'
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='登録情報を入力してください')
+            )
 
-    #     conn.commit()
+    elif event.message.text == '確認':
+        STATUS = '確認'
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='確認したい人の名前を教えてください')
+            )
 
-    #     select_sql = 'select * from userinfo'
-
-    #     c.execute(select_sql)
-    #     result = c.fetchone()
-    #     conn.close()
-    #     message =''
-    dbname = 'info.db'
-
-    conn = sqlite3.connect(dbname)
-    c = conn.cursor()
-    message = ''
-    select_sql = 'select * from userinfo'
-    c.execute(select_sql)
-    result = c.fetchone()
-    conn.close()
-
-    for text in result:
-        message=message+str(text)
-    line_bot_api.reply_message(
+    if STATUS == '登録':
+        STATUS = ''
+        message = register(event)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=message)
+            )
+    elif STATUS == '確認':
+        STATUS == ''
+        name = event.message.text
+        message = ''
+        result = check(name)
+        for text in result:
+            message=message+str(text)+' '
+        line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=message)
             )
 
+    line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='行いたい操作を選択してください(e.g. 登録 確認)')
+            )
+
+def split(event):
+    return 'test'
+
+def check(name):
+    dbname = 'info.db'
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    select_sql = 'select * from userinfo where id like %'+name+'%'
+    c.execute(select_sql)
+    result = c.fetchone()
+    conn.close()
+    return result
+
+
+def register(event):
+    dbname = 'info.db'
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    '''test'''
+    id = 2
+    name = 'yuhi matsuo'
+    height = 165
+    weight = 58
+    dateofbirth = '1998/12/08'
+    personality = '真面目'
+    '''--------'''
+    sql = 'insert into userinfo (id, name, height, weight, dateofbirth, personality) values (?,?,?,?,?,?)'
+    info = (id, name, height, weight, dateofbirth, personality)
+    conn.execute(sql, info)
+    conn.commit()
+    conn.close()
+    return '完了'
 
 if __name__ == "__main__":
-#    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
