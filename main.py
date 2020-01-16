11fad830b66b84816c9484667d7350055bf93b4e
IsHYuhi <yuhiregend@icloud.com>
2020-01-16 16:25:57 +0900

    add button & function test

--- main.py
+++ main.py
@@ -45,21 +45,50 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     global STATUS
+
     if event.message.text == '登録':
-        STATUS = '登録'
+        button_message = add_info()
+        line_bot_api.reply_message(event.reply_token,
+                                  button_message
+                                  )
+    elif event.message.text == '確認':
+        button_message = confirm()
+        line_bot_api.reply_message(event.reply_token,
+                                  button_message
+                                  )
+
+
+
+    if event.message.text == '新規登録':
+        STATUS = '新規登録'
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=STATUS+'情報を入力してください')
             )
 
-    elif event.message.text == '確認':
-        STATUS = '確認'
+    elif event.message.text == '利用者情報確認':
+        STATUS = '利用者情報確認'
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=STATUS+'したい人の名前を教えてください')
             )
+
+    elif event.message.text == '削除':
+        STATUS = '削除'
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='情報を'+STATUS+'したい人の名前を教えてください')
+            )
+
+    elif event.message.text == '追加':
+        STATUS = '追加'
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='情報を'+STATUS+'したい人の名前を教えてください')
+            )
+
     else:
-        if STATUS == '登録':
+        if STATUS == '新規登録':
             message = register(event.message.text)
             line_bot_api.reply_message(
                 event.reply_token,
@@ -67,7 +96,7 @@ def handle_message(event):
                 )
             STATUS = ''
 
-        elif STATUS == '確認':
+        elif STATUS == '利用者情報確認':
             name = event.message.text
             message = ''
             result = check(name)
@@ -81,7 +110,38 @@ def handle_message(event):
                 TextSendMessage(text=message)
                 )
             STATUS = ''
-        
+
+        elif STATUS == '削除':
+            name = event.message.text
+            message = ''
+            result = delete_info(name)
+
+            if result == None:
+                message = 'その人は存在しません'
+            else:
+                message = result
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=message)
+                )
+            STATUS = ''
+
+        elif STATUS == '追加':
+            name = event.message.text
+            message = ''
+            result = update_info(name)
+
+            if result == None:
+                message = 'その人は存在しません'
+            else:
+                message = result
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=message)
+                )
+            STATUS = ''
+
+
         button_message = make_button_template()
         line_bot_api.reply_message(
                 event.reply_token,
@@ -114,6 +174,14 @@ def register(text):
     conn.close()
     return '完了'
 
+def delete_info(text):
+    #TODO
+    return '完了'
+
+def update_info(text):
+    #TODO
+    return '完了'
+
 def extract(text):
     table=re.sub('介護者情報書\n|1利用者氏名\n|2身長\(cm\)\(cmは記入不要\)\n|3体重\(kg\)\(kgは記入不要\)\n|4生年月日\(西暦\)\n|5持病\n|6特徴\n','',text)
     new_table=table.splitlines()
@@ -133,7 +201,7 @@ def extract(text):
     DOB=re.sub('年|月|日','',DOB_tmp2)
 
     return name, height, weight, DOB, personality + '\n' + disease
-    
+
 
 def make_button_template():
     message_template = TemplateSendMessage(
@@ -154,13 +222,51 @@ def make_button_template():
                     text="削除"
                 ),
                 MessageAction(
-                    label="確認",
-                    text="確認"
+                    label="追加",
+                    text="追加"
                 ),
             ]
         )
     )
     return message_template
+
+def confirm():
+    confirm_button = TemplateSendMessage(
+        alt_text="どちらの情報を確認しますか",
+        template=ButtonsTemplate(
+            text="どちらの情報を確認しますか",
+            actions=[
+                MessageAction(
+                    label="利用者情報",
+                    text="利用者情報"
+                ),
+                MessageAction(
+                    label="介護日誌確認",
+                    text="介護日誌確認"
+                ),
+            ]
+        )
+    )
+    return confirm_button
+
+def add_info():
+    add_button = TemplateSendMessage(
+        alt_text="どちらの情報を登録しますか",
+        template=ButtonsTemplate(
+            text="どちらの情報を登録しますか",
+            actions=[
+                MessageAction(
+                    label="新規登録",
+                    text="新規登録"
+                ),
+                MessageAction(
+                    label="介護日誌登録",
+                    text="介護日誌登録"
+                ),
+            ]
+        )
+    )
+    return add_button
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
